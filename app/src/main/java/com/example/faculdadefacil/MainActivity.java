6585540e57c06b0c9a8670ddc13015d0bc19faee
luizmps <luiz_adolfo10@hotmail.com>
2020-09-25 20:19:06 -0300

    FragmentScreen
    Fragment Screns implementation

--- app/src/main/java/com/example/faculdadefacil/MainActivity.java
+++ app/src/main/java/com/example/faculdadefacil/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.faculdadefacil;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
@@ -20,15 +28,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        //Remove native NightTheme On App
         AppCompatDelegate.setDefaultNightMode(
                 AppCompatDelegate.MODE_NIGHT_NO);
 
-        setContentView(R.layout.activity_main);
-
+        //Import Navigation and Drawer
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
 
-        
+        //Drawer Open on click dashboard pic
+        /*findViewById(R.id.dashboard_img_pic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });*/
+
+        //Controle de Telas Fragment
+        NavController navController = Navigation.findNavController(this,R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 }

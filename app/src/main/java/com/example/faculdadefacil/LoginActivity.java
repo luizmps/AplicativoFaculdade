c5923c2a26617c6b4071b6cc5852cf4e4d1bd66e
luizmps <luiz_adolfo10@hotmail.com>
2020-11-09 11:39:00 -0300

    newRequests

--- app/src/main/java/com/example/faculdadefacil/LoginActivity.java
+++ app/src/main/java/com/example/faculdadefacil/LoginActivity.java
@@ -8,10 +8,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.material.navigation.NavigationView;
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.ServerError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email_ET, password_ET;
+    private String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +39,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button btn = (Button)findViewById(R.id.LoginButton);
+        email_ET = findViewById(R.id.RaAluno);
+        password_ET = findViewById(R.id.SenhaAluno);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                email = email_ET.getText().toString();
+                password = password_ET.getText().toString();
+
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 finish();
             }
         });
     }
+
+
 }
